cb9e4215e645cd450474e15e5860b27a779746f5
John <ibrokhim1155@email.com>
2024-10-17 19:10:47 +0500

    texnomart-uz

--- config/urls.py
+++ config/urls.py
@@ -15,17 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenRefreshView
+from config.jwt_views import MyTokenObtainPairView
 from config import settings
+from config import custom_obtainviews
+
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('texnomart-uz/', include('texnomart.urls')),
+                  path('api-token-auth/', custom_obtainviews.CustomAuthToken.as_view()),
+                  path('api/token/access/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
